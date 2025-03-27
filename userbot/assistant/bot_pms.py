537a45817962e2ee2cab4e293f3d48c516c0992c
DRECO <rishabh.private.in@gmail.com>
2025-03-28 02:26:16 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -116,13 +116,13 @@ async def bot_start(event):  # sourcery skip: low-code-quality
             start_msg = f"Hey! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
                         \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
+                        \n\nPowered by [userbot](https://t.me/rishabh_zz)"
         buttons = [
             (
-                Button.url("Repo", "https://github.com/TgCatUB/catuserbot"),
+                Button.url("Repo", "https://www.youtube.com/shorts/Haj0wvhS4Vo"),
                 Button.url(
                     "Deploy",
-                    "https://github.com/TgCatUB/nekopack",
+                    "https://www.youtube.com/shorts/Haj0wvhS4Vo",
                 ),
             )
         ]
